8034ec26d73f8da1dc9ad6f137ab20d40547ffbb
jasset <juanantonioaguilar@gmail.com>
2015-10-10 03:01:30 +0200

    Revert "1st load"
    This reverts commit 8fe6601689b7b4567d2ccd0e94d3f07596cf47e0.

--- public/js/general.js
+++
@@ -1,9 +0,0 @@
-"use strict"
-
-//setting up to start a new game
-function newGame() {
-  if (window.localStorage) {
-    localStorage.setItem('ghostword', '');
-    localStorage.setItem('rookie', false);       
-  }
-}
--- test/isNotEmptySpec.js
+++
@@ -1,9 +0,0 @@
-describe("isNotEmpty", function() {
-  it("returns true when the object, array or string is not an empty value, null or undefined", function(){
-    expect(isNotEmpty({ test: 'A' })).toEqual(true);
-  });
-
-  it("returns false when the object, array or string is an empty value, null or undefined", function(){
-    expect(isNotEmpty({})).toEqual(false);
-  });
-});
